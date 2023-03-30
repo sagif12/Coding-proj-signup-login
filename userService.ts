d8565cb135c1e8966793400a26c35f0b06e61647
sagif12 <97915973+sagif12@users.noreply.github.com>
2023-03-30 17:03:28 -0500

    Delete userService.ts

--- userService.ts
+++
@@ -1,9 +0,0 @@
-import { getRecord } from "./db.ts";
-
-export async function getUser(email: string) {
-  const rec = await getRecord(email);
-  if (!rec) {
-    throw new Deno.errors.NotFound();
-  }
-  return rec;
-}
